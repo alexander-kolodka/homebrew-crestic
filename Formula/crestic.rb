54cc5bb8c0695d5d278b3dc203e7f53e67ff652d
Alexander Kolodka <alexander.kolodka.2210@gmail.com>
2026-04-06 23:12:06 +0700

    v0.0.7

--- Formula/crestic.rb
+++ Formula/crestic.rb
@@ -1,8 +1,8 @@
 class Crestic < Formula
   desc "Wrapper around restic with YAML-configured jobs"
   homepage "https://crestic.kolodka.fyi/"
-  url "https://github.com/alexander-kolodka/crestic/archive/refs/tags/v0.0.6.tar.gz"
-  sha256 "9b5cc34522c72ce64fb24703b51e23a07cd298f0525617d7fbf8842942c95073"
+  url "https://github.com/alexander-kolodka/crestic/archive/refs/tags/v0.0.7.tar.gz"
+  sha256 "1becb7c4b7da1e9f6deeeed9771b43e153f688ff52cc33cd9069b3b8fb3efad7"
   license "MIT"
 
   depends_on "go" => :build
